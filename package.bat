46bbc999f0e02c90148b171a39755e437502b3e3
Andy Jenkins <andy.jenkins@rhythmagency.com>
2019-08-22 18:40:07 -0700

    Well that didn't work. Let's put a specific version of the library in

--- package.bat
+++ package.bat
@@ -1,3 +1,3 @@
 cd src
-nuget.exe pack Rhythm.Cors\Rhythm.Cors.csproj -Build -OutputDirectory ..\dist -Properties Configuration=Release
+lib\nuget.exe pack Rhythm.Cors\Rhythm.Cors.csproj -Build -OutputDirectory ..\dist -Properties Configuration=Release
 pause
